c225c526250923ffa8228c5698e29a973967f9bd
Voltage789 <ivanov.kir2001@gmail.com>
2022-09-18 00:16:21 +0300

    2

---
+++ app/src/main/java/com/company/nemov/PassingTest.kt
@@ -0,0 +1,11 @@
+package com.company.nemov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PassingTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passing_test)
+    }
+}
---
+++ app/src/main/java/com/company/nemov/SubjAdapter.kt
@@ -0,0 +1,4 @@
+package com.company.nemov
+
+class SubjAdapter {
+}
